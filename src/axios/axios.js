1000a8de0246365e6c84e91b784ca01052504c10
unknown <dracsharp@gmail.com>
2020-09-21 13:29:36 +0100

    api key as variable

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,8 +1,9 @@
+import { youtubeAPIKey } from "../youtubeAPI/youtubeAPIKey";
 const axios = require("axios");
 export const getHomeVideos = () =>
 	axios
 		.get(
-			"https://www.googleapis.com/youtube/v3/search?maxResults=12&key=AIzaSyADDiAUfSlRc40K8ZwqsMrsYHs_yuogcXo"
+			`https://www.googleapis.com/youtube/v3/search?maxResults=12&key=${youtubeAPIKey}`
 		)
 		.then((response) => response.data.items)
 		.catch((error) => console.log("error", error));
